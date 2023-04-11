393b5b8ab9c21254ec87d93c810d9ed8bbea1ba2
Alina Tatianchikova <tataynchikovalina@gmail.com>
2023-04-11 11:36:37 +0300

    11.04 edited comments in first commit edited lost test_data file

---
+++ locators/test_data.py
@@ -0,0 +1,7 @@
+class TestData:
+    name = 'Алина'
+    surname = 'Татьянчикова'
+    address = 'Ломоносовский проспект 24'
+    phone_number = '79134562341'
+    dzen_url = 'https://dzen.ru/?yredirect=true'
+    base_url = 'https://qa-scooter.praktikum-services.ru/'
